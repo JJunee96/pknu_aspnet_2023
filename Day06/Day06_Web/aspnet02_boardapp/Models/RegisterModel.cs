0a15caebadb5ff046ec6c1e23ec6da08eb7e390e
JJunee96 <wnsltkak@naver.com>
2023-05-24 17:26:10 +0900

    23-05-24

---
+++ Day06/Day06_Web/aspnet02_boardapp/Models/RegisterModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+
+namespace aspnet02_boardapp.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        [EmailAddress]
+        [DisplayName(Name = "이메일주소")]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [DisplayName(Name = "패스워드")]
+        public string Password { get; set; }
+        [DataType(DataType.Password)]
+        [DisplayName(Name = "패스워드 확인")]
+        [Compare("Password", ErrorMessage = "패스워드가 일치하지 않습니다. 다시 입력하세요.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
